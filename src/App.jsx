6ed7d8acf26102f51dfcf3da785d6164eaf8f718
Milo9869 <milo.sottilini@gmail.com>
2024-10-26 11:29:21 +0200

    Ajout des cours des 10 principales crypto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import LeverageCalculator from './components/LeverageCalculator';
+import CryptoTracker from './components/CryptoTracker';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md mx-auto">
+      <div className="max-w-4xl mx-auto space-y-8">
+        <CryptoTracker />
         <LeverageCalculator />
       </div>
     </div>
